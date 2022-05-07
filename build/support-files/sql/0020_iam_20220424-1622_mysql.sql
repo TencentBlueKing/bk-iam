44feff8750c670e434b88f5b61abdca26d901284
nannan00 <17491932+nannan00@users.noreply.github.com>
2022-05-07 14:48:43 +0800

    fix(sql): missing semicolon

--- build/support-files/sql/0020_iam_20220424-1622_mysql.sql
+++ build/support-files/sql/0020_iam_20220424-1622_mysql.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `bkiam`.`saas_action` ADD COLUMN `sensitivity` TinyInt(3) unsigned NOT NULL DEFAULT 0 AFTER `type`;
-ALTER TABLE `bkiam`.`saas_resource_type` ADD COLUMN `sensitivity` TinyInt(3) unsigned NOT NULL DEFAULT 0 AFTER `provider_config`
+ALTER TABLE `bkiam`.`saas_resource_type` ADD COLUMN `sensitivity` TinyInt(3) unsigned NOT NULL DEFAULT 0 AFTER `provider_config`;
