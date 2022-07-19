5abeb4763b685968ab0f89c07f16cdffc846accc
Timmy <zhu327@gmail.com>
2022-07-19 20:19:27 +0800

    feat(rbac_group_resource_policy): add rbac sync tables (#156)

--- build/support-files/sql/0024_iam_20220523-0740_mysql.sql
+++ build/support-files/sql/0024_iam_20220523-0740_mysql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `bkiam`.`group_resource_policy` (
+CREATE TABLE IF NOT EXISTS `bkiam`.`rbac_group_resource_policy` (
    `pk` int(10) unsigned NOT NULL AUTO_INCREMENT,
    `signature` CHAR(32) NOT NULL,
    `group_pk` int(10) unsigned NOT NULL,
@@ -12,5 +12,6 @@ CREATE TABLE IF NOT EXISTS `bkiam`.`group_resource_policy` (
    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    PRIMARY KEY (`pk`),
    UNIQUE KEY `idx_uk` (`signature`),
-   INDEX `idx_resource` (`resource_id`(9), `action_related_resource_type_pk`, `resource_type_pk`, `system_id`)
+   INDEX `idx_resource` (`resource_id`(9), `action_related_resource_type_pk`, `resource_type_pk`, `system_id`),
+   KEY `idx_group_action_resource_type` (`group_pk`,`action_related_resource_type_pk`,`system_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='policy with resource';
