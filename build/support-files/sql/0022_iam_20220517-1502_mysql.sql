5bedc0fd378e8a49ae87d136f38905b69cb40a4a
Timmy <zhu327@gmail.com>
2022-08-08 15:13:36 +0800

    build: add worker/checker command (#168)

--- build/support-files/sql/0022_iam_20220517-1502_mysql.sql
+++ build/support-files/sql/0022_iam_20220517-1502_mysql.sql
@@ -5,10 +5,15 @@ ALTER TABLE `bkiam`.`subject_relation` DROP INDEX `idx_parent`;
 ALTER TABLE `bkiam`.`subject_relation` DROP INDEX `idx_subject_pk_expire`;
 
 -- drop column
-ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `subject_type`;
-ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `subject_id`;
-ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `parent_type`;
-ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `parent_id`;
+-- ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `subject_type`;
+-- ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `subject_id`;
+-- ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `parent_type`;
+-- ALTER TABLE `bkiam`.`subject_relation` DROP COLUMN `parent_id`;
+
+ALTER TABLE `bkiam`.`subject_relation` MODIFY COLUMN `subject_type` varchar(16) NOT NULL DEFAULT '';
+ALTER TABLE `bkiam`.`subject_relation` MODIFY COLUMN `subject_id` varchar(64) NOT NULL DEFAULT '';
+ALTER TABLE `bkiam`.`subject_relation` MODIFY COLUMN `parent_type` varchar(16) NOT NULL DEFAULT '';
+ALTER TABLE `bkiam`.`subject_relation` MODIFY COLUMN `parent_id` varchar(64) NOT NULL DEFAULT '';
 
 -- create index
 CREATE UNIQUE INDEX `idx_uk_parent_subject` ON `bkiam`.`subject_relation` (`parent_pk`,`subject_pk`);
