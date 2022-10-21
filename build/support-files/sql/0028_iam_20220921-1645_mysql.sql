9259c6680abbb3f43481960fd9ab1576b4eabace
Timmy <zhu327@gmail.com>
2022-10-21 16:46:06 +0800

    bugfix: restore subject_relation table field policy_expired_at (#211)

--- build/support-files/sql/0028_iam_20220921-1645_mysql.sql
+++ build/support-files/sql/0028_iam_20220921-1645_mysql.sql
@@ -1,1 +1,1 @@
-CREATE INDEX `idx_expire_subject_parent` ON `bkiam`.`subject_relation` (`expired_at`,`subject_pk`,`parent_pk`);
+CREATE INDEX `idx_expire_subject_parent` ON `bkiam`.`subject_relation` (`policy_expired_at`,`subject_pk`,`parent_pk`);
